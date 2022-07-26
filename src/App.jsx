e0e63cb38655851b1244a44d7389579266162654
chocopros <jesuschock93@gmail.com>
2022-07-25 20:59:30 -0400

    start-2

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,21 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [statePass, setpass] = useState(false)
+
+  const toggleState = () => setpass(!statePass)
 
   return (
-    <div className="container">
 
+    <div className="container">
+      <div className="info-password">
+        <h1>Password Info</h1>
+        <div className="input-info">
+          <label htmlFor=""><b>Constraseña: </b></label>
+          <input type={statePass ? "text" : "password"} />
+          <button onClick={toggleState}><i class='bx bx-low-vision'></i></button>
+        </div>
+      </div>
     </div>
   )
 }
