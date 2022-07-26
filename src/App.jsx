284b0a3883cef9be31568bfab9f67c76790d2e9a
chocopros <jesuschock93@gmail.com>
2022-07-25 22:00:53 -0400

    start-4

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import './App.css'
 
 function App() {
   const [statePass, setpass] = useState(false)
-
   const toggleState = () => setpass(!statePass)
+
+  //CONTADOR
+
+  const [count, setCount] = useState(0)
+  const handlePlus = () => {setCount(count +1)}
+  const handlePlusRest = () => {setCount(state =>  state-1)} //Otra forma
+  const handlePlusReset = () => {setCount(0)}
 
   return (
 
@@ -14,8 +20,16 @@ function App() {
         <h1>Password Info</h1>
         <div className="input-info">
           <label htmlFor=""><b>Constraseña: </b></label>
-          <input type={statePass ? "text" : "password"} />
-          <button onClick={toggleState}><i class='bx bx-low-vision'></i></button>
+          <input className='input-pass' type={statePass ? "text" : "password"} />
+          <button className='btn' onClick={toggleState}><i class='bx bx-low-vision'></i></button>
+        </div>
+      </div>
+      <div className="info-password">
+        <h1>{count}</h1>
+        <div className="input-info">
+          <button className='btn' onClick={handlePlus}>+1</button>
+          <button className='btn' onClick={handlePlusRest}>-1</button>
+          <button className='btn' onClick={handlePlusReset}>reset</button>
         </div>
       </div>
     </div>
