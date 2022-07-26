3e3fa1b51d6cfafa4898fd0c6904be09ab730c49
chocopros <jesuschock93@gmail.com>
2022-07-25 22:42:36 -0400

    start-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect } from 'react';
 import './App.css'
 
 function App() {
@@ -13,15 +13,34 @@ function App() {
   const handlePlusRest = () => {setCount(state =>  state-1)} //Otra forma
   const handlePlusReset = () => {setCount(0)}
 
+  //background
+  const [color, setColor] = useState()
+
+  const colors = ["#845EC2", "#D65DB1", "#FF6F91", "#FF9671", "#FFC75F", "#F9F871"];
+
+  const getRandomElement = arr => {
+    const randomIndex = Math.floor(Math.random() * arr.length)
+    return arr[randomIndex]
+  }
+  
+  useEffect(() => {
+    const randomColor = getRandomElement(colors)
+    setColor(randomColor)
+  }, [statePass])
+
+  
+
+  
+
   return (
 
-    <div className="container">
+    <div className='container' style={{backgroundColor: color}}>
       <div className="info-password">
         <h1>Password Info</h1>
         <div className="input-info">
           <label htmlFor=""><b>Constraseña: </b></label>
-          <input className='input-pass' type={statePass ? "text" : "password"} />
-          <button className='btn' onClick={toggleState}><i class='bx bx-low-vision'></i></button>
+          <input  className='input-pass' type={statePass ? "text" : "password"} />
+          <button  className='btn' onClick={toggleState}><i className='bx bx-low-vision'></i></button>
         </div>
       </div>
       <div className="info-password">
